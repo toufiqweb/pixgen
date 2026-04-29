5b142ec388e80efcfaacfe6821c11cd0dd857e76
toufiq178 <tajahmedtoufiq1@gmail.com>
2026-04-29 16:04:21 +0600

    final deploy

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -14,10 +14,14 @@ export const auth = betterAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
-     github: { 
-            clientId: process.env.GITHUB_CLIENT_ID , 
-            clientSecret: process.env.GITHUB_CLIENT_SECRET , 
-        },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    },
+    facebook: {
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    },
   },
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
